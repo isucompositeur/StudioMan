d9119a3f3bc671be7fc5df7f220052d3ac9d3f52
Nick Meyer <nick@mesosynoptic.com>
2011-03-25 18:05:12 -0500

    Playing around

---
+++ StudioMan/SMSourceListController.h
@@ -0,0 +1,17 @@
+//
+//  SMSourceListController.h
+//  StudioMan
+//
+//  Created by Nicholas Meyer on 3/24/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface SMSourceListController : NSController {
+@private
+    
+}
+
+@end
---
+++ StudioMan/SMSourceListController.m
@@ -0,0 +1,29 @@
+//
+//  SMSourceListController.m
+//  StudioMan
+//
+//  Created by Nicholas Meyer on 3/24/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "SMSourceListController.h"
+
+
+@implementation SMSourceListController
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+@end
